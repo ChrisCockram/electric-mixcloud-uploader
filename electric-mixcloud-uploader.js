c1a7f6665ad492c09376d5b64f0cf5a598e55c41
Chris Cockram <chris.cockram@gmail.com>
2021-07-17 19:26:51 +0100

    Update to UTC

--- electric-mixcloud-uploader.js
+++ electric-mixcloud-uploader.js
@@ -66,32 +66,7 @@ function checkFTP(){
 
 const checkFTPInterval = setInterval(checkFTP, 5000);
 
-let current_show= {
-    date: "2021-05-27",
-    start: "10:00",
-    end: "11:00",
-    show: {
-        id: 9360,
-        name: "Electric Anthems",
-        slug: "anthems",
-        url: "https://electricradio.co.uk/show/anthems/",
-        latest: "",
-        website: "",
-        hosts: [ ],
-        producers: [ ],
-        genres: [
-            "Classic House"
-        ],
-        languages: [ ],
-        avatar_url: "https://electricradio.co.uk/wp-content/uploads/2020/04/electric-anthems-150x150.jpg",
-        image_url: "https://electricradio.co.uk/wp-content/uploads/2020/04/electric-anthems-150x150.jpg",
-        route: "https://electricradio.co.uk/wp-json/radio/shows/?show=anthems",
-        feed: "https://electricradio.co.uk/feed/shows/?show=anthems"
-    },
-    encore: false,
-    split: false,
-    override: false
-};
+
 current_show=false;
 //Get Current Show
 function getShow(){
@@ -121,8 +96,8 @@ function request_show(show){
         logger.info('This show is not on the include list');
         return false;
     }
-    start = moment(new Date(show.date +' '+show.start));
-    end = moment(new Date(show.date +' '+show.end));
+    start = moment.utc(new Date(show.date +' '+show.start));
+    end = moment.utc(new Date(show.date +' '+show.end));
     show.start_date=start;
     if(show.end<show.start){
         end.add(1, 'day');
